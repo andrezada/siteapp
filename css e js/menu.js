b26afa57372d1178de24f0d915383c2643ecbe8f
André Luís de M. Rodrigues <serrimanco@gmail.com>
2025-09-07 16:56:53 -0300

    gostaria de uma limodada

--- css e js/menu.js
+++ css e js/menu.js
@@ -5,3 +5,11 @@
     hamburger.classList.toggle('active');
     navUl.classList.toggle('active');
   });
+
+      document.querySelector('.conta').addEventListener('click', function() {
+      document.querySelector('.login').style.visibility = 'visible';
+      });
+
+      document.getElementById('fechar').addEventListener('click', function() {
+      document.querySelector('.login').style.visibility = 'hidden';
+      });
