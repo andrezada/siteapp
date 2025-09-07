4b8b932a5c7ab94e64d86c2ced5fa1d481ab3bc3
gabrii3l <gabrielmluz56@gmail.com>
2025-09-07 20:23:51 -0300

    Delete css e js/menu.js

--- css e js/menu.js
+++
@@ -1,7 +0,0 @@
-
-  const hamburger = document.getElementById('hamburger-menu');
-  const navUl = document.querySelector('nav ul');
-  hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('active');
-    navUl.classList.toggle('active');
-  });
