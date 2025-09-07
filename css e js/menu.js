923839c56961327b845a27b1770ab65bb1369870
André Luís de M. Rodrigues <serrimanco@gmail.com>
2025-09-07 17:44:56 -0300

    talvez as entidades se pareçam comigo

--- css e js/menu.js
+++ css e js/menu.js
@@ -2,6 +2,12 @@
   const hamburger = document.getElementById('hamburger-menu');
   const navUl = document.querySelector('nav ul');
   hamburger.addEventListener('click', function() {
+     const ferramentas = document.querySelector('.ferramentas'); 
+      if (ferramentas.style.visibility === 'hidden') {
+        ferramentas.style.visibility = 'hidden';
+      } else {
+        ferramentas.style.visibility = 'hidden';
+      }
     hamburger.classList.toggle('active');
     navUl.classList.toggle('active');
   });
@@ -12,4 +18,14 @@
 
       document.getElementById('fechar').addEventListener('click', function() {
       document.querySelector('.login').style.visibility = 'hidden';
+      });
+
+
+      document.getElementById('menuferramentas').addEventListener('click', function() {
+      const ferramentas = document.querySelector('.ferramentas'); 
+      if (ferramentas.style.visibility === 'visible') {
+        ferramentas.style.visibility = 'hidden';
+      } else {
+        ferramentas.style.visibility = 'visible';
+      }
       });
